861c9510bad880c32e9e821dcdc7ccb7152c615e
KifilweJasmin <137528030+KifilweJasmin@users.noreply.github.com>
2023-08-28 01:36:38 +0200

    Showing the current temperature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-alert("hello");
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "25d874c323417eb6fa36f60bca44be24";
+let lat = -26.186332588;
+let lon = 28.318998724;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
